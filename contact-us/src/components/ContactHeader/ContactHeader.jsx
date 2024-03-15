024bce05cb1e64716c058ffd00e1a2dc16cea3a7
Soumya <soumyashru11@gmail.com>
2024-03-16 00:49:59 +0530

    new projects

---
+++ contact-us/src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import styles from './ContactHeader.module.css'
+
+const ContactHeader = () => {
+  return (
+    <div className={`${styles.contact_section}`}>
+        <h1>CONTACT US</h1>
+        <p></p>
+    </div>
+  )
+}
+
+export default ContactHeader
